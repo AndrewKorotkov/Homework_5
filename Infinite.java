162c46a79576f3dc866832600997641e7862be4e
Andrew <123314186+AndrewKorotkov@users.noreply.github.com>
2023-03-05 16:37:06 +0300

    Task_One

---
+++ Infinite.java
@@ -0,0 +1,15 @@
+package homework_five;
+
+import java.util.Scanner;
+
+public class Infinite {
+
+    public static void main(String[] args) {
+        
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.next();
+            System.out.println(s);
+        }
+    }
+}
